ba862f5a9f6de915c155decd1d2d36a24a7d8854
Eddy <eddy.lazar@gmail.com>
2019-08-19 19:32:08 +0300

    github ribbon

--- src/front/App.tsx
+++ src/front/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import UrlsContainer from './components/UrlsContainer';
+import GithubRibbon from './components/GithubRibbon';
 import config from '../config';
 
-const App = () => <UrlsContainer urls={config.urls} />;
+const App = () => (
+  <div>
+    <GithubRibbon />
+    {/* <h1>Healthcheck</h1> */}
+    <UrlsContainer urls={config.urls} />
+  </div>
+);
 
 export default App;
